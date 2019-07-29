fa9688a0b65d26be81cfffc85e2419c0917e39b1
Alexey_Lisenko <detoner777@gmail.com>
2019-07-29 18:54:20 +0300

    Starting use Express

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes);
+const app = express();
+
+app.use((req, res, next) => {
+	console.log('In the midleware');
+	next();//allows the request to continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+	console.log('In anothe middleware!');
+	res.send('<h1>Hello from Express!</h1>');
+})
+
+const server = http.createServer(app);
 
 server.listen(3000);
