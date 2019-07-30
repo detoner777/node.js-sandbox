ec3a7c2684c708c4070b452c48b5a2640ea57b89
Alexey_Lisenko <detoner777@gmail.com>
2019-07-30 12:49:31 +0300

    GET and POST routs

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-	console.log('In anothe middleware!');
-	res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product Name</button></form>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res, next) => {
-	console.log(req.body);
-	res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-	console.log('In anothe middleware!');
-	res.send('<h1>Hello from Express!</h1>');
+app.use((req, res, next) => {
+	res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
