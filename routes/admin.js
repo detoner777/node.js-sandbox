905392ed895c2ae9c36d8bb7ef3267406ada91ac
Alexey_Lisenko <detoner777@gmail.com>
2019-07-30 17:40:19 +0300

    CSS adding

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,14 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-	res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product Name</button></form>');
+	res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
